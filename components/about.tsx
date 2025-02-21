60226b44e02b1ee4ba12e80e3722fd0d36523362
vanshaj932 <karnvanshaj123@gmail.com>
2025-02-22 01:08:48 +0530

    final 2

--- components/about.tsx
+++ components/about.tsx
@@ -82,12 +82,12 @@ const AboutHuggies: React.FC = () => {
           <h2 className="text-3xl font-semibold text-gray-800">
             Experience the Huggies Difference
           </h2>
-          <p className="mt-4 text-gray-600">
+          <p className="mt-4 mb-4 text-gray-600">
             Give your baby the best care with Huggies diapers and wipes.
           </p>
           <Link
             href="/product"
-            className="mt-6 px-6 py-3 bg-red-500 text-white font-semibold rounded-full shadow-md hover:bg-red-600 transition"
+            className="mt-10 px-6 py-3 bg-red-500 text-white font-semibold rounded-full shadow-md hover:bg-red-600 transition"
           >
             Shop Now
           </Link>
