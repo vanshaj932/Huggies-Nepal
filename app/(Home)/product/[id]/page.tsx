836658f026dfe01f6980c5cf14e84f2d5ca452b4
vanshaj932 <karnvanshaj123@gmail.com>
2025-02-23 19:30:39 +0530

    fixed dynamic route

--- app/(Home)/product/[id]/page.tsx
+++ app/(Home)/product/[id]/page.tsx
@@ -17,14 +17,10 @@ export async function generateStaticParams(): Promise<PageParams[]> {
 }
 
 // Page component
-const ProductPage = async ({
-  params,
-}: {
-  params: PageParams;
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) => {
-  const productId = parseInt(params.id, 10);
-  const product = products.find((p) => p.id === productId);
+const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const productId = (await params).id;
+  const productID = parseInt(productId, 10);
+  const product = products.find((p) => p.id === productID);
 
   if (!product) {
     return <div>Product not found</div>;
